14f8599e8e1835f6a41d2ea0d1aaaa28dbffaf42
Zhang Sen <skyzs2001@vip.qq.com>
2022-07-03 21:03:24 +0800

    trying to fix lb

--- playground/src/cpu.scala
+++ playground/src/cpu.scala
@@ -141,7 +141,7 @@ class cpu extends Module {
     )),
     0.U
   )
-  io.data_sram_addr  := tlb.io.out.addr
+  io.data_sram_addr  := Cat(tlb.io.out.addr(31, 2), 0.U(2.W))
   io.data_sram_wdata := reg.io.out.rt_data
 
   val mem_addrrd = (idu_exu.io.idu_contr_out.mem_read & (~pause) & idu_exu.io.valid_out & MuxLookup(idu_exu.io.idu_contr_out.mem_mask, false.B, Array(
@@ -187,20 +187,16 @@ class cpu extends Module {
   ))
   reg.io.in.rd_data   := Mux(mem_reg.io.idu_contr_out.mem_read,
     MuxLookup(mem_reg.io.idu_contr_out.mem_mask, 0.U, Array(
-      1.U -> Cat(Fill(24, mem_reg.io.mem_data_out.rdata(7) & mem_reg.io.idu_contr_out.signed.asUInt()),
-        MuxLookup(tlb.io.out.addr(1, 0), 0.U, Array(
-          "b00".U -> mem_reg.io.mem_data_out.rdata( 7,  0),
-          "b01".U -> mem_reg.io.mem_data_out.rdata(15,  8),
-          "b10".U -> mem_reg.io.mem_data_out.rdata(23, 16),
-          "b11".U -> mem_reg.io.mem_data_out.rdata(31, 24)
-        ))
-      ),
-      2.U -> Cat(Fill(16, mem_reg.io.mem_data_out.rdata(15) & mem_reg.io.idu_contr_out.signed.asUInt()),
-        MuxLookup(tlb.io.out.addr(1, 0), 0.U, Array(
-          "b00".U -> mem_reg.io.mem_data_out.rdata(15,  0),
-          "b10".U -> mem_reg.io.mem_data_out.rdata(31, 16)
-        ))
-      ),
+      1.U -> MuxLookup(tlb.io.out.addr(1, 0), 0.U, Array(
+          "b00".U -> Cat(Fill(24, mem_reg.io.mem_data_out.rdata( 7) & mem_reg.io.idu_contr_out.signed.asUInt()), mem_reg.io.mem_data_out.rdata( 7,  0)),
+          "b01".U -> Cat(Fill(24, mem_reg.io.mem_data_out.rdata(15) & mem_reg.io.idu_contr_out.signed.asUInt()), mem_reg.io.mem_data_out.rdata(15,  8)),
+          "b10".U -> Cat(Fill(24, mem_reg.io.mem_data_out.rdata(23) & mem_reg.io.idu_contr_out.signed.asUInt()), mem_reg.io.mem_data_out.rdata(23, 16)),
+          "b11".U -> Cat(Fill(24, mem_reg.io.mem_data_out.rdata(31) & mem_reg.io.idu_contr_out.signed.asUInt()), mem_reg.io.mem_data_out.rdata(31, 24))
+      )),
+      2.U -> MuxLookup(tlb.io.out.addr(1, 0), 0.U, Array(
+          "b00".U -> Cat(Fill(16, mem_reg.io.mem_data_out.rdata(15) & mem_reg.io.idu_contr_out.signed.asUInt()), mem_reg.io.mem_data_out.rdata(15,  0)),
+          "b10".U -> Cat(Fill(16, mem_reg.io.mem_data_out.rdata(31) & mem_reg.io.idu_contr_out.signed.asUInt()), mem_reg.io.mem_data_out.rdata(31, 16))
+      )),
       3.U -> mem_reg.io.mem_data_out.rdata
     )),
     Mux(mem_reg.io.idu_contr_out.call_src,
